b9735ca0125b25ac956d1fe6981789fa0578670a
Gaydamaka <sasagajdamaka641@gmail.com>
2024-11-09 13:57:24 +0200

    Update main.cpp

--- Конструкторы/main.cpp
+++ Конструкторы/main.cpp
@@ -1,15 +1,17 @@
 #include "Phone.h"
 #include "Lamp.h"
 #include "Banknote.h"
+#include "Cat.h"
 
 int main() {
     Phone myPhone;
     Lamp lamp;
     Banknote myBanknote;
-
+    Cat cat;
+    
     myPhone.MakeCall();
     lamp.TurnOn();
     myBanknote.Deposit();
-
+    cat.Play();
     return 0;
 }
